0a9be6472d62c32bd5466167904ba758621a4a37
ashish nagar <ashishnagar033@gmail.com>
2023-03-24 11:20:24 +0530

    delete functonality added

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleDeleteClick = async () => {
+    const response = await fetch(
+      "https://mern-workout-app.vercel.app/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -12,6 +29,9 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps (kg): </strong>
         {workout.reps}
       </p>
+      <span className="deleteBtn" onClick={handleDeleteClick}>
+        delete
+      </span>
     </div>
   );
 };
